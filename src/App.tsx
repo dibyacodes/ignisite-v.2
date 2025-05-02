e217ae3979abc889b48e49bbb3885dec15b637d3
Dibyajyoti Kumar <134666822+dibyacodes@users.noreply.github.com>
2025-05-02 05:23:55 +0000

    UPDATE : writting a message for esrver integration

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-inter font-semibold underline">
-        Hello world!
+        Perform server side rendering
+
+        {/* server side rendering mainly consists of running an express server and to lod the entire app documen to the server and the loading it to the main browser page
+        
+        using server side rendering increases the website speed as well as bring about cpde security and other useful security measures into account while taking care of the website's SEO
+
+        */}
       </h1>
     </>
   )
