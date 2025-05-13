8d47f3288aa4869c22ddad331150a3982d3d2721
dibyacodes <dibyacodes@gmail.com>
2025-05-13 08:14:36 +0530

    Formatted tsx code

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -33,42 +33,43 @@ function Home() {
             <div className="w-[100%] z-10 h-svh pb-10 px-2 flex flex-col bg-radial-[at_50%_0%] from-[#E100FF] to-black to-70% justify-center gap-5 ">
                 
                 <div className="flex flex-col gap-2">
-                    <h1 className="text-5xl font-semibold font-inter max-w-[20ch] text-left mt-10 text-transparent bg-clip-text bg-gradient-to-r from-gray-100 via-purple-200 to-gray-100">
-                        Scaling your business begins here.
-                    </h1>
-                    <p  className="max-w-[30ch] md:flex text-left text-gray-200 leading-tight text-lg">
-                        <span className="font-inter font-medium text-md capitalize">
-                            plan it. build it. ship it.
-                        </span>
-                    </p>
-                </div>
+					<h1 className="text-5xl font-semibold font-inter max-w-[20ch] text-left mt-10 text-transparent bg-clip-text bg-gradient-to-r from-gray-100 via-purple-200 to-gray-100">
+						Scaling your business begins here.
+					</h1>
+					<p  className="max-w-[30ch] md:flex text-left text-gray-200 leading-tight text-lg">
+						<span className="font-inter font-medium text-md capitalize">
+							plan it. build it. ship it.
+						</span>
+					</p>
+				</div>
+
 
 				<div>
-				<List subject="we help you with" items={list}/>
+					<List subject="we help you with" items={list}/>
 				</div>
-                <div className="flex flex-col gap-3">
-                    <button className="border-2 border-gray-400/50 capitalize font-semibold rounded-full text-white font-inter text-lg px-2 py-2">
-                        Book a Call
-                    </button>
+				<div className="flex flex-col gap-3">
+					<button className="border-2 border-gray-400/50 capitalize font-semibold rounded-full text-white font-inter text-lg px-2 py-2">
+						Book a Call
+					</button>
 
 
-                    <button className="font-semibold rounded-full text-gray-200 border-2 border-gray-400/50 font-inter text-lg px-2 py-2">
-                        {
-                            "View Services ->"
-                        }
-                    </button>
-                </div>
+					<button className="font-semibold rounded-full text-gray-200 border-2 border-gray-400/50 font-inter text-lg px-2 py-2">
+						{
+							"View Services ->"
+						}
+					</button>
+				</div>
 
-            </div>
-            <div className="bg-gray-950 h-screen flex flex-col items-center pt-10">
-                <div>
-                    {/* {
-                        servicesArray.map((items, index) => (
-                            <Card key={index} title={items.title} description={items.description} />
-                        ))
-                    } */}
-                </div>
-            </div>
+			</div>
+			<div className="bg-gray-950 h-screen flex flex-col items-center pt-10">
+				<div>
+					{/* {
+						servicesArray.map((items, index) => (
+							<Card key={index} title={items.title} description={items.description} />
+						))
+					} */}
+				</div>
+			</div>
         </>
     )
 }
