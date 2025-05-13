f282f3f46fb003470d3ed774d2641dae29fcf2aa
dibyacodes <dibyacodes@gmail.com>
2025-05-13 21:04:24 +0530

    Added card component

--- src/ui/components/Card.tsx
+++ src/ui/components/Card.tsx
@@ -1,33 +1,46 @@
 
 interface cardDetails {
     title: string,
-    description: string,
-    buttonText?: string
+    description ?: string[],
+
 }
 
 
-function Card({ title, description, buttonText }: cardDetails) {
+function Card({ title, description }: cardDetails) {
     return (
         <>
-            <div className="max-w-[50ch] bg-gray-200 rounded-lg p-4 w-[35ch] border-2 border-b-6">
-                <div className="flex flex-col gap-5">
-                    <h1 className="text-2xl text-green-900 font-semibold font-inter leading-tight">
-                        {title}
-                    </h1>
+            <div className=" flex flex-col gap-5 bg-radial-[at_50%_100%] from-gray-500 to-black to-70% border-1 border-gray-700 w-[90%] p-4 rounded-2xl">
+                <div className="flex flex-row items-center justify-between">
+                    <div className="w-[50%]">
+                        {/* card title */}
+                        <h1 className="text-gray-200 font-inter leading-tight text-xl">
+                            {/* There's a website for every idea
+                                 */}
 
-                    <p className="font-inter flex gap-10 font-medium text-md">
-                        {
-							description
-						}
-                    </p>
+                            {title}
+                        </h1>
+                    </div>
+                    <div className="bg-gray-200/80 rounded-full flex flex-col items-center h-fit ">
+                        {/* stats */}
+                        <h1 className="text-gray-900 font-inter px-2">Save 50%</h1>
+                        {/* <p>services</p> */}
+                    </div>
                 </div>
 
-                <div hidden={
-                    buttonText ? false : true
-                }>
-                    <button>
-                        {buttonText}
-                    </button>
+                <div className="flex flex-wrap gap-5">
+
+                    {
+                        description?.map((items) => (
+                            <div className="bg-gray-300 w-[45%] flex flex-row items-center gap-4 px-4 py-1 justify-between rounded-full">
+                                <div className="font-inter text-sm">
+                                    {items}
+                                </div>
+                                <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </>
--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,21 +1,31 @@
 // import Card from "../components/Card"
+import Card from "../components/Card"
 import List from "../components/List"
 
-
+// testing
 
 function Home() {
 
-    // const servicesArray = [
-    //     {
-    //         title: "Custom Website Development",
-    //         description: ['Making', 'Element', 'Advanced'],
-    //     },
+	// const servicesArray = [
+	//     {
+	//         title: "Custom Website Development",
+	//         description: ['Making', 'Element', 'Advanced'],
+	//     },
 
-    //     {
-    //         title: "Website Redesign & Maintenance",
-    //         description: "check your website buddy"
-    //     }
-    // ]
+	//     {
+	//         title: "Website Redesign & Maintenance",
+	//         description: "check your website buddy"
+	//     }
+	// ]
+
+	const services =[
+		'Ecommerce',
+		'Portfolio',
+		'Education',
+		'Restaurant',
+		'Personal',
+		'Product'
+	]
 
 	const list = [
 		'Custom Website Designs',
@@ -24,45 +34,51 @@ function Home() {
 		'SEO Optimization Services'
 	]
 
-    // install node and other packages to make this app work
+	// install node and other packages to make this app work
 
-    return (
-        // bg-[radial-gradient(to_bottom,#E100FF_0%,black_30%)]
-        <>
-        {/* bg-radial-[at_x_y] */}
-            <div className="w-[100%] z-10 h-svh pb-10 px-2 flex flex-col bg-radial-[at_50%_0%] from-[#E100FF] to-black to-70% justify-center gap-5 ">
-                
-                <div className="flex flex-col gap-2">
-					<h1 className="text-5xl font-semibold font-inter max-w-[20ch] text-left mt-10 text-transparent bg-clip-text bg-gradient-to-r from-gray-100 via-purple-200 to-gray-100">
+	return (
+		// bg-[radial-gradient(to_bottom,#E100FF_0%,black_30%)]
+		<>
+			{/* bg-radial-[at_x_y] */}
+			<div className="w-[100%] z-10 h-svh pb-10 px-2 flex flex-col bg-radial-[at_50%_0%] from-[#E100FF] to-black to-70% justify-center gap-5 ">
+
+				<div className="flex flex-col gap-2">
+					<h1 className="text-5xl font-semibold font-inter max-w-[20ch] text-left mt-10 text-transparent bg-clip-text bg-gradient-to-r from-gray-100 via-purple-300 to-gray-100">
+
 						Scaling your business begins here.
 					</h1>
-					<p  className="max-w-[30ch] md:flex text-left text-gray-200 leading-tight text-lg">
+					<p className="max-w-[30ch] md:flex text-left text-gray-200 leading-tight text-lg">
 						<span className="font-inter font-medium text-md capitalize">
 							plan it. build it. ship it.
 						</span>
 					</p>
 				</div>
-
-
 				<div>
-					<List subject="we help you with" items={list}/>
+					<List subject="we help you with" items={list} />
 				</div>
 				<div className="flex flex-col gap-3">
 					<button className="border-2 border-gray-400/50 capitalize font-semibold rounded-full text-white font-inter text-lg px-2 py-2">
 						Book a Call
 					</button>
-
-
-					<button className="font-semibold rounded-full text-gray-200 border-2 border-gray-400/50 font-inter text-lg px-2 py-2">
-						{
-							"View Services ->"
-						}
-					</button>
 				</div>
-
 			</div>
-        </>
-    )
+
+			{/* <div className="bg-black px-4">
+				<div className="flex flex-col gap-4">
+					<h1 className="text-purple-200 font-inter text-4xl capitalize font-medium">
+						an internet home for your business
+					</h1>
+					<p className="text-gray-400 font-inter font-medium text-md leading-tight">
+						Let your customers access you & your brand online, while you focus on your vision.
+					</p>
+				</div>
+			</div> */}
+
+			<div className="bg-black flex flex-col justify-center items-center">
+				<Card title="There's a website for every idea" description={services}/>
+			</div>
+		</>
+	)
 }
 
 export default Home
