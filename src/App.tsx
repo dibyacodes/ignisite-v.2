5fbf8e479c49563cc9dfbc078d103eb50ac79546
dibyacodes <dibyacodes@gmail.com>
2025-06-30 09:49:28 +0530

    Removed the pop up message and added a link functionality to the button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,36 @@
 import { Outlet } from "react-router"
-import { useEffect, useState } from "react";
-import Pop from "./components/Pop";
+// import { useEffect, useState } from "react";
+// import Pop from "./components/Pop";
 
 function App() {
-  const [showDialog, setShowDialog] = useState(false);
-  const [dialogDismissed, setDialogDismissed] = useState(false);
+//   const [showDialog, setShowDialog] = useState(false);
+//   const [dialogDismissed, setDialogDismissed] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrolled = window.scrollY + window.innerHeight;
-      const totalHeight = document.documentElement.scrollHeight;
-      const percent = scrolled / totalHeight;
-      if (percent >= 0.6 && !dialogDismissed) {
-        setShowDialog(true);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [dialogDismissed]);
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const scrolled = window.scrollY + window.innerHeight;
+//       const totalHeight = document.documentElement.scrollHeight;
+//       const percent = scrolled / totalHeight;
+//       if (percent >= 0.6 && !dialogDismissed) {
+//         setShowDialog(true);
+//       }
+//     };
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, [dialogDismissed]);
 
-  const handleClose = () => {
-    setShowDialog(false);
-    setDialogDismissed(true);
-  };
+//   const handleClose = () => {
+//     setShowDialog(false);
+//     setDialogDismissed(true);
+//   };
 
-  return (
+
+return (
     <>
-
       <Outlet />
-      <Pop open={showDialog} onClose={handleClose}>
-        {/* Your advertisement or content here */}
-        <h2>Needs More Design!</h2>
-        <p>As of now this is the pop up. there will designs and images coming in</p>
-      </Pop>
-      {/* Your page content */}
     </>
-  )
+)
 }
+
 
 export default App
