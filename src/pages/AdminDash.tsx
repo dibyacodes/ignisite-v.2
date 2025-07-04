655ce483b92773fc18515e88005bef3e4163c421
dibyacodes <dibyacodes@gmail.com>
2025-07-04 13:00:33 +0530

    code tweak test

--- src/pages/AdminDash.tsx
+++ src/pages/AdminDash.tsx
@@ -16,7 +16,7 @@ function AdminDash() {
     const [databaseData, setDatabaseDate] = useState([])
 
     useEffect(() => {
-        axios.get('https://25d3-2405-201-a805-e01c-10b7-23b7-3cac-dcdc.ngrok-free.app/admin/allbookings').then((res) => setDatabaseDate(res.data.message)).catch((err) => console.log(err))
+        axios.get('https://25d3-2405-201-a805-e01c-10b7-23b7-3cac-dcdc.ngrok-free.app/admin/allbookings').then((res) => setDatabaseDate(res)).catch((err) => console.log(err))
         console.log(databaseData)
     }, [])
     return (
